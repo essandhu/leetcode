a1b23a3897fcc7f3b6a9b604d9230c8d8e952bf9
Erick Singh Sandhu <essandhu22@gmail.com>
2026-04-09 11:09:49 -0500

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequencyMap = {}
+        # numbersByOccurences[i] = list of each number with frequency "i"
+        numbersByOccurences = [[] for i in range(len(nums) + 1)]
+        
+        for number in nums:
+            frequencyMap[number] = frequencyMap.get(number, 0) + 1
+        
+        # key = number itself, value = frequency of number
+        for key, value in frequencyMap.items():
+            numbersByOccurences[value].append(key)
+
+        def getMostFrequent(nestedList: List[List[int]]) -> List[int]:
+            mostFrequentList = []
+
+            for frequency in reversed(nestedList):
+                for value in frequency:
+                    mostFrequentList.append(value)
+                    if len(mostFrequentList) == k:
+                        return mostFrequentList
+
+            return []
+
+        return getMostFrequent(numbersByOccurences)
